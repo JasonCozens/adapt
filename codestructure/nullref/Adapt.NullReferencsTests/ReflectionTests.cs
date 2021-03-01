f18871432302eed85b96171c234568c61b7b9f97
Jason Cozens <e.jason.cozens@gmail.com>
2021-03-01 19:21:30 +0000

    Parameter type test.

--- codestructure/nullref/Adapt.NullReferencsTests/ReflectionTests.cs
+++ codestructure/nullref/Adapt.NullReferencsTests/ReflectionTests.cs
@@ -46,9 +46,21 @@ namespace Adapt.NullReferencsTests
 
             var constructors = reflectionClass.GetConstructors();
 
-            Assert.Equal(2, constructors.Count());
+            Assert.Equal(2, constructors.Length);
             Assert.Equal(1, constructors.Count(c => c.GetParameters().Length == 0));
             Assert.Equal(1, constructors.Count(c => c.GetParameters().Length == 1));
+        }
+
+        [Fact]
+        public void GetParameterType()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var reflectionClass = assembly.GetType("Adapt.NullReferencsTests.ReflectionClass");
+            var constructor = reflectionClass.GetConstructors().First(c => c.GetParameters().Length == 1);
+
+            var parameter = constructor.GetParameters()[0];
+
+            Assert.Equal(typeof(IReflect), parameter.ParameterType);
         }
     }
 }
